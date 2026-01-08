0876863e46bab423eceb02bed43ecfb2ad1f4b42
presenceshopp-create <presenceshopp@gmail.com>
2026-01-08 17:23:16 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StoreController::class, 'index']);
-Route::get('/produto/{product}', [StoreController::class, 'show']);
-Route::post('/carrinho/add/{id}', [CartController::class, 'add']);
-Route::get('/carrinho', [CartController::class, 'index']);
+Route::get('/', function () {
+    return 'Laravel está rodando corretamente 🚀';
+});
